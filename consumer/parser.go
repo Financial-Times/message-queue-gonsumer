8c3e7dc71abf01f05f4dd5667765546291a344f9
Zoltan Bodor <Zoltan.Bodor@ft.com>
2016-04-25 17:04:43 +0300

    remove log

--- consumer/parser.go
+++ consumer/parser.go
@@ -47,7 +47,6 @@ func parseMessage(raw string) (m Message, err error) {
 		return
 	}
 	doubleNewLineStartIndex := getHeaderSectionEndingIndex(string(decoded[:]))
-	log.Printf("INFO  - index [%d]\n", doubleNewLineStartIndex)
 	if m.Headers, err = parseHeaders(string(decoded[:doubleNewLineStartIndex])); err != nil {
 		return
 	}
